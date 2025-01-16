b26c45166b603398e8429b82fda897c90c23aa53
Ruby Tarnawa <lena.thorne04@gmail.com>
2025-01-16 10:57:38 +0100

    added .yml with venv, progress in object recognition

--- object_recognition/from-saved-model.py
+++ object_recognition/from-saved-model.py
@@ -22,9 +22,9 @@ def download_images():
     filenames = ['image1.jpg', 'image2.jpg']
     image_paths = []
     for filename in filenames:
-        image_path = tf.keras.utils.get_file(fname = filename,
-                                             origin=base_url + filename,
-                                             untar=False)
+        image_path = tf.keras.utils.get_file(fname=filename,
+                                            origin=base_url + filename,
+                                            untar=False)
         image_path = pathlib.Path(image_path)
         image_paths.append(str(image_path))
     return image_paths
@@ -44,19 +44,35 @@ MODEL_DATE = '20200711'
 MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
 PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
 
+# Downloading labels
+def download_labels(filename):
+    base_url= 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
+    label_dir = tf.keras.utils.get_file(fname=filename,
+                                        origin=base_url + filename,
+                                        untar=False)
+    label_dir = pathlib.Path(label_dir)
+    return str(label_dir)
+
+LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
 # imports for loading the model
 import time
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
 
-PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR = "/saved_model"
+PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR + "/centernet_hg104_1024x1024_coco17_tpu-32/saved_model"
 
 print('Loading model...', end='')
 start_time = time.time()
 
-# Load saved model and build detection
+# Load saved model and build the detection function
 detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print('Done! Took {} seconds'.format(elapsed_time))
+
+# Loading labels for plotting
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
