6d1fd16541d2e4552cf36975c23568dbe5769973
Ruby Tarnawa <lena.thorne04@gmail.com>
2025-01-20 12:57:49 +0100

    losing hope, another tutorial with a more clearly defined dataset

---
+++ object_recognition/example_or.py
@@ -0,0 +1,7 @@
+# This document will use the following tutorial
+# https://www.tensorflow.org/tfmodels/vision/object_detection
+#
+# I pray to every god. Please let this go smoothly.
+#
+# As per usual, this is intended for ues with an Anaconda environment.
+# Upon completion, the relevant yml file shall be included in the repo
